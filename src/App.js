8391fe288b1c7ef3f742a98b5d320e8cc475d073
avkash <kukadiyaavkash01@gmail.com>
2025-04-17 11:17:33 +0530

    16-4-2025 work

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from "./logo.svg";
+import "./App.css";
+// import "./componet/Navbar.js";
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  // let abx = "hey rilay "
+  // let name  =" the king";
+  return (
+    <>
+      <Navbar/>
+    </>
+  );
+}
+
+export default App;
